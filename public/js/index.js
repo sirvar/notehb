0dcd1a65aea34b15a10c9d8492e9041afbbf10a7
Rikin Katyal <katyalrikin@gmail.com>
2016-09-17 04:26:30 -0400

    Teacher login

--- public/js/index.js
+++ public/js/index.js
@@ -12,8 +12,9 @@ firebase.initializeApp(config);
 $(document).ready(function() {
 	var provider = new firebase.auth.GoogleAuthProvider();
 	var database = firebase.database();
-	
-	$("#signin-student").click(function() {
+
+	$("#signin-teacher").click(function() {
+		console.log("yea");
 		firebase.auth().signInWithPopup(provider).then(function(result) {
 		  // This gives you a Google Access Token. You can use it to access the Google API.
 		  var token = result.credential.accessToken;
@@ -28,6 +29,7 @@ $(document).ready(function() {
 		  var email = error.email;
 		  // The firebase.auth.AuthCredential type that was used.
 		  var credential = error.credential;
+		  console.log(errorMessage);
 		});
 	});
 });
--- public/teacher/js/index.js
+++ public/teacher/js/index.js
@@ -13,13 +13,22 @@ $(document).ready(function() {
 	var provider = new firebase.auth.GoogleAuthProvider();
 	var database = firebase.database();
 	
-	$("#signin-student").click(function() {
+	$("#signin-teacher").click(function() {
 		firebase.auth().signInWithPopup(provider).then(function(result) {
-		  // This gives you a Google Access Token. You can use it to access the Google API.
-		  var token = result.credential.accessToken;
-		  // The signed-in user info.
-		  var user = result.user;
-		  console.log(user);
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			var token = result.credential.accessToken;
+			// The signed-in user info.
+			var user = result.user;
+			// Gen class code
+			var classCode = genClassCode();
+
+			return firebase.database().ref('/teachers/' + user.uid).once('value')
+			.then(function(snapshot) {
+				if (snapshot.val() == null) {
+					addTeacher(user.uid, user.displayName, user.email, classCode);
+				}
+			});
+
 		}).catch(function(error) {
 		  // Handle Errors here.
 		  var errorCode = error.code;
@@ -28,6 +37,25 @@ $(document).ready(function() {
 		  var email = error.email;
 		  // The firebase.auth.AuthCredential type that was used.
 		  var credential = error.credential;
+		  console.log(errorMessage)
 		});
 	});
 });
+
+
+function addTeacher(userId, name, email, classCode) {
+	firebase.database().ref('teachers/' + userId).set({
+		username: name,
+		email: email,
+		classCode : classCode
+	});
+}
+
+function genClassCode() {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for( var i=0; i < 8; i++ )
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;
+}
