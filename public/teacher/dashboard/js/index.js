9345f7847422b4c0d4d68eac1db94d04e61e7c66
Rikin Katyal <katyalrikin@gmail.com>
2016-09-17 16:01:57 -0400

    added voice

--- public/teacher/dashboard/js/index.js
+++ public/teacher/dashboard/js/index.js
@@ -13,11 +13,132 @@ $(document).ready(function() {
 	var database = firebase.database();
 	var uid = getParameterByName("uid");
 
+	var seconds = 0;			//Time elapsed in seconds
+	var minutes = 0;
+	var hours = 0;
+	var paused = true;
+	var stopped = false;
+	var n;
+	var time = "";
+	$("#stop").css("opacity", "0.2");
+
+	window.setInterval(function() {
+		if (!paused && !stopped) {
+			seconds += 1;
+			if (seconds >= 60) {
+				seconds = 0;
+				minutes += 1;
+			}
+			if (minutes >= 60) {
+				minutes = 0;
+				hours += 1
+			}
+			document.getElementById("time-elapsed").innerHTML = hours + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + 
+				(seconds < 10 ? "0" + seconds : seconds);
+		}
+	}, 1000);
+
+	$("#play-pause").click(function() {
+		if ($(this).attr("value") == "paused" && !(paused = paused == stopped) && !stopped) {
+			$(this).attr("value", "playing");
+			$(this).html('<img src="../../img/pause.png" alt="A pause button" />');
+			$("#stop").css("opacity", 1);
+			startListening(null);
+		}
+		else {
+			paused = true;
+			$(this).attr("value", "paused");
+			if (!stopped)
+				$(this).html('<img src="../../img/play.png" alt="A play button" />');
+			startListening(null);
+		}
+	})
+	$("#stop").click(function () {
+		var playpause = $("#play-pause");
+		if (!stopped && playpause.attr("value") == "playing") {
+			playpause.attr("value", "paused");
+			playpause.html('<img src="../../img/play.png" alt="A play button" />');
+		}
+		if (seconds != 0 || minutes != 0 || hours != 0) {
+			stopped = true;
+			$(playpause).html('<img src="../../img/play.png" alt="A play button" />');
+			$(playpause).css("opacity", "0.2");
+			$(this).css("opacity", 0.2);
+		}
+	})
+
+
+	// Check if browser supports WebSpeech API
+	if (!('webkitSpeechRecognition' in window)) {
+	  alert("Your browser doesn't support the Web Speech API :(");
+	} else {
+	  var recognizing = false;
+	  var recognition = new webkitSpeechRecognition();
+	  recognition.continuous = true;
+	  recognition.interimResults = true;
+
+	  function startListening(event) {
+	    if (recognizing) {
+	      recognition.stop();
+	    }else {
+	      recognition.lang = "en-CA";
+	      recognition.start();
+	    }
+	  }
+	}
+	// Create Stop Button once WebSpeech API starts
+	  recognition.onstart = function() {
+	    recognizing = true;
+	  };
+
+	  // Create Start Button once WebSpeech API ends
+	  recognition.onend = function() {
+	    recognizing = false;
+	  };  
+
+	    // Display WebSpeech API errors
+	  recognition.onerror = function(event) {
+	    if (event.error == 'no-speech') {
+	      alert('No speech was detected');
+	    }
+	    else if (event.error == 'audio-capture') {
+	      alert('Could not detect a microphone on your device');
+	    }
+	    else if (event.error == 'not-allowed') {
+	      alert('Cannot access your microphone. Did you grant permissions?');
+	    }
+	    else {
+	      alert('Unknown Error with Speech Recognition - ' + event.error);
+	    }
+	  };
+
+	  // Handle WebSpeech API results
+	  recognition.onresult = function(event) {
+	    var interim_transcript = '';
+	    var final_transcript = '';
+	        if (typeof(event.results) == 'undefined') {
+	          recognition.onend = null;
+	          recognition.stop();
+	          upgrade();
+	          return;
+	        }
+	        for (var i = event.resultIndex; i < event.results.length; ++i) {
+	          if (event.results[i].isFinal) {
+	            final_transcript += event.results[i][0].transcript;
+	          } else {
+	            interim_transcript += event.results[i][0].transcript;
+	          }
+	        }
+	        $("#textarea").append(final_transcript);
+	  };
+
+
 	return firebase.database().ref('/teachers/' + uid).once('value')
 	.then(function(snapshot) {
 		var user = snapshot.val();
 		$("#classcode").text("Class Code:\n" + user.classCode);
 	});
+
 });
 
 function getParameterByName(name, url) {
